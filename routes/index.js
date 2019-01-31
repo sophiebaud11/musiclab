74d81c6865768c122641b09e6265902083a28e5e
mk <mk@mks-MacBook-Pro.local>
2019-01-31 17:14:18 -0500

    Updates: player added

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/samples/:sampleId', function(req, res, next) {
   console.log("req params are " + JSON.stringify(req.params));
   var counter = ("0000" + (req.params.sampleId+1)).slice(-4);
   var soundUrl = ("https://s3.amazonaws.com/ll-musiclab/public/mp3_samples/sample_" + counter + ".mp3")
-  res.render('instrument', { title: 'the secret musicLab', sampleId: counter, theSoundUrl: soundUrl });
+  res.render('instrument', { title: ('Sample ' + counter), sampleId: counter, theSoundUrl: soundUrl });
 });
 
 module.exports = router;
