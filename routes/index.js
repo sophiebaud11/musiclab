b67456cd94dde71efa62dea31bd7b314ce94e842
mk <mk@mks-MacBook-Pro.local>
2019-01-31 16:47:44 -0500

    Updates= intial commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('the secret musicLab', { title: 'the secret musicLab' });
+  res.render('index', { title: 'the secret musicLab' });
 });
 
 module.exports = router;
