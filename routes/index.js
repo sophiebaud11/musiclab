bda2bb9cf44104ccce9b9da39466da4cfd160145
mk <mk@mks-MacBook-Pro.local>
2019-02-01 05:30:01 -0500

    Updates:

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,27 @@ router.get('/samples/:sampleId', function(req, res, next) {
   res.render('instrument', { title: ('Sample ' + counter), sampleId: counter, theSoundUrl: soundUrl });
 });
 
+
+
+router.get('/randomsamples', function(req, res, next) {
+  var sampleNumbers = []
+  for (var i = 0; i < 5438; i++) {
+    sampleNumbers.push(("0000" + (i+1)).slice(-4));
+  }
+  var randomSampleIds = [];
+  for (var i = 0; i < 16; i++) {
+    var theIndex = Math.floor(Math.random() * sampleNumbers.length);
+    var theElement = sampleNumbers.splice(theIndex, 1);
+    randomSampleIds.push(theElement);
+  }
+  console.log("these are the randomSampleIds: " + randomSampleIds);
+  var randomSampleUrls = [];
+  for (var i = 0; i < randomSampleIds.length; i++) {
+    randomSampleUrls.push(("https://s3.amazonaws.com/ll-musiclab/public/mp3_samples/sample_" + randomSampleIds[i] + ".mp3"));
+  }
+  console.log("these are the randomSampleUrls: " + randomSampleUrls);
+  res.render('grid-controller', { title: ("These are your samples"), sampleIds: randomSampleIds, randomSampleUrls: randomSampleUrls });
+})
+
+
 module.exports = router;
